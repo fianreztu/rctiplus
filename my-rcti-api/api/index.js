6af999927ab4c41c38807ec9d88e14294711fabc
fianreztu <161385389+fianreztu@users.noreply.github.com>
2026-07-29 10:28:43 +0800

    Change export syntax from default to module.exports

--- my-rcti-api/api/index.js
+++ my-rcti-api/api/index.js
@@ -1,6 +1,6 @@
 const axios = require('axios');
 
-export default async function handler(req, res) {
+module.exports = async (req, res) => {
   try {
     const headers = {
       "User-Agent": "Mozilla/5.0 (Linux; Android 12; Pixel 6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Mobile Safari/537.36",
@@ -31,14 +31,12 @@ export default async function handler(req, res) {
       return res.status(500).send("URL Stream tidak ditemukan");
     }
 
-    // Set Header agar Vercel meng-cache hasilnya selama 45 menit 
-    // (mengurangi beban request ke API RCTI+ dan mempercepat respon)
+    // Cache Vercel 45 menit
     res.setHeader('Cache-Control', 's-maxage=2700, stale-while-revalidate');
     
-    // Kembalikan URL stream m3u8 ber-token
     return res.status(200).send(m3u8Url);
 
   } catch (err) {
     return res.status(500).send("Error API: " + err.message);
   }
-}
+};
